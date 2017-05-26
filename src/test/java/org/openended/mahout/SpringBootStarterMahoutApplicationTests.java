0e834c9b323344d9ba995f741f88d5742757f341
Jens Fischer <j.fischer@epages.com>
2017-05-26 23:32:23 +0200

    start.spring.io

---
+++ src/test/java/org/openended/mahout/SpringBootStarterMahoutApplicationTests.java
@@ -0,0 +1,16 @@
+package org.openended.mahout;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootStarterMahoutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
